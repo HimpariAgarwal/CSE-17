6fcc1528546c65347a654e879d5279d8fbb04ff3
HimpariAgarwal <himpariag3105@gmail.com>
2026-06-23 18:36:09 +0530

    Add files via upload
    Experiment1

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("Responsive Design Experiment Loaded");
+
+let year=new Date().getFullYear();
+
+document.querySelector("footer p").innerHTML=
+"© "+year+" Responsive Design Experiment";
